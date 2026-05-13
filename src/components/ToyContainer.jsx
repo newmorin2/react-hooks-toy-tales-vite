63d04ccaecc8905d2c832bf1db40c57120f155a9
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-05-13 09:43:21 +0300

    displaying all toys

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, []);
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {toys.map((toy) => (
+        <ToyCard key={toy.id} toy={toy} />
+      ))}
+    </div>
   );
 }
 
